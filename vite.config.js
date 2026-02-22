18639badaaf8e1c7ce99ac330c0277d8539a90a2
magicman <107155783+Magic-Man-us@users.noreply.github.com>
2026-02-22 17:53:09 -0500

    feat: refactor GitHub Actions workflow and update Vite configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === 'build' ? '/HowMuchHouseCanIAfford/' : '/',
+  base: '/',
   build: {
     sourcemap: false,
     minify: 'terser',
@@ -14,4 +14,4 @@ export default defineConfig(({ command }) => ({
       },
     },
   },
-}));
+});
